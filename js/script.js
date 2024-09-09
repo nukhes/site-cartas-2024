041f0b80d9b06dd8b78febed709c3fcfab022111
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2024-09-09 20:51:51 +0000

    34rt34t

--- js/script.js
+++ js/script.js
@@ -3,16 +3,42 @@ const main = document.querySelector("#main");
 var actualIndex = 0;
 
 const urlList = [
-  "./card/00.jpg",
-  ["./card/01.jpg", "./card/02.jpg"],
-  ["./card/03.jpg", "./card/04.jpg", "./card/05.jpg"],
-  ["./card/06.jpg", "./card/07.jpg", "./card/08.jpg", "./card/09.jpg"],
-  ["./card/10.jpg", "./card/11.jpg", "./card/12.jpg"],
-  ["./card/13.jpg", "./card/14.jpg"],
+  "./card/-1.jpg",
+  [
+    "./card/00.jpg",
+    [
+      "./card/01.jpg",
+      "./card/02.jpg",
+    ],
+    [
+      "./card/03.jpg",
+      "./card/04.jpg",
+    ],
+    [
+      "./card/05.jpg",
+      "./card/06.jpg",
+    ],
+  ],
+  [
+    "./card/00.jpg",
+    [
+      "./card/10.jpg",
+      "./card/02.jpg",
+    ],
+    [
+      "./card/03.jpg",
+      "./card/04.jpg",
+    ],
+    [
+      "./card/05.jpg",
+      "./card/06.jpg",
+    ],
+  ],
   "./card/99.jpg"
 ];
 
-var path = [1, 1, 1, 1, 1];
+
+var path = [1, 1, 1, 1];
 
 // Adiciona o evento de clique para as cartas
 refreshCards(1);
@@ -20,39 +46,30 @@ refreshCards(1);
 
 function addListener(e) { 
   e.addEventListener("click", a = () => {
-    if (actualIndex == urlList.length) {
-      alert("Fim da história");
+    if (actualIndex > 5) {
+      alert("Fim.");
       showAll();
     } else {
-      console.log(actualIndex)
-      if (actualIndex == 0 || actualIndex == 6) {
+      if (actualIndex == 0 || actualIndex == 5) {
         chooseCard(1);
-      } else if (actualIndex == 1 || actualIndex == 5) {
+      } else {
         chooseCard(2);
       }
-      else if (actualIndex == 3) {
-        chooseCard(4);
-      }
-      else if (actualIndex == 2 || actualIndex == 4) {
-        chooseCard(3);
-      }
-      else if (actualIndex == 4) {
-        chooseCard(4);
-      }
     }
-  })
+    
+  });
 }
 
 function chooseCard(n) {
-  actualIndex++;
+  console.log(actualIndex);
   refreshCards(n);
+  actualIndex++;
 }
 
 function refreshCards(n) {
   input.innerHTML = "";
   for (let i = 0; i < n; i++) {
     input.innerHTML += `<div style="background-color: ${getRandomColor()};" class="card stroked" src="" alt="" srcset="">?</div>`;
-    addListener(input.lastElementChild);
   }
   
   var divs = input.getElementsByClassName("card");
@@ -70,5 +87,7 @@ function getRandomColor() {
 }
 
 function showAll() {
+  main.innerHTML = '';
+  input.innerHTML = '';
   main.innerHTML += `<img class="card" src="${urlList[actualIndex]}">`
 }
